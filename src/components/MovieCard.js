a3f3d341273d40f40810a23f3ee34819c031054c
eclecticcoding <chuck@eclecticsaddlebag.com>
2020-02-11 20:29:01 -0500

    Set up card layout and mapped genres from ids to names with local JS object.

---
+++ src/components/MovieCard.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Card, CardImg, CardTitle, CardBody, CardText, Badge } from 'reactstrap'
+import { Link } from 'react-router-dom'
+
+import genreMap from '../data/movieGenres'
+import { MOVIE_DB_IMAGE_URL } from '../data/apiMovies'
+import noPhoto from '../assets/images/noimage.png'
+
+const MovieCard = ({ id, title, genre_ids, poster_path, average_vote }) => {
+  let genresStr = ''
+  if (genre_ids) {
+    genresStr = genre_ids
+      .map(id => {
+        const item = genreMap.genres.find(item => item.id === id)
+        return item ? item.name : null
+      })
+      .join(', ')
+  }
+  console.log(genresStr)
+  return (
+    <div>
+      <Card className="movie-card">
+        <CardImg
+          top
+          width="100%"
+          src={poster_path ? `${MOVIE_DB_IMAGE_URL.medium}${poster_path}` : noPhoto}
+          alt={title}
+        />
+        <CardBody>
+          <Link to={`/movies/${id}`}>
+            {average_vote > 0 && <span className="card-rating">{average_vote}</span>}
+            <CardTitle className="mb-1 mr-4">{title}</CardTitle>
+            {genresStr && <p className="small mb-0">{genresStr}</p>}
+          </Link>
+        </CardBody>
+      </Card>
+    </div>
+  )
+}
+
+export default MovieCard
--- src/containers/MovieList.js
+++ src/containers/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchMovies } from '../actions/index'
+import MovieCard from '../components/MovieCard'
 
 class MovieList extends Component {
   componentDidMount() {
@@ -8,13 +9,13 @@ class MovieList extends Component {
   }
 
   render() {
-    const movies = this.props.movies.map((movie, index) => (
-      <li key={index}>{movie.title}</li>
-    ))
+    const movies = this.props.movies
+      .slice(0, 20)
+      .map((movie, index) => <MovieCard key={index} movie={movie} {...movie} />)
+    console.log(this.props.movies.id)
     return (
-      <div>
-        <h1>Hello from the Movie List</h1>
-        <p>{movies}</p>
+      <div className="d-flex flex-wrap justify-content-md-between justify-content-center">
+        {movies}
       </div>
     )
   }
