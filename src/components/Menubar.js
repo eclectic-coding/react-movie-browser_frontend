abd4bbc9754337900b2e561b1d43d6a5c1844f29
eclecticcoding <chuck@eclecticsaddlebag.com>
2020-02-14 06:00:44 -0500

    Add React Fontawesome and add icon for nav brand.

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -1,11 +1,49 @@
 import React from 'react'
+import {
+  Button,
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavLink
+} from 'react-bootstrap'
+import { NavLink as NavLinkRouter } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faVideo } from '@fortawesome/free-solid-svg-icons'
 
 const Menubar = () => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-      <h1 className="text-black">Movie Browser</h1>
-      <button color="primary">Login</button>
-    </div>
+    <Navbar className="fixed-top" bg="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">
+          <FontAwesomeIcon
+            icon={faVideo}
+            aria-hidden="false"
+            className="text-warning brand-icon"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Form inline>
+            <FormControl
+              disabled
+              type="text"
+              placeholder="Future Search Controller"
+              className="mr-sm-2"
+            />
+          </Form>
+        </Navbar.Collapse>
+        <Nav.Item>
+          <NavLinkRouter to="/login" exact>
+            <Button color="danger" className="btn-round btn-sign">
+              SIGN IN
+            </Button>
+          </NavLinkRouter>
+          )}
+        </Nav.Item>
+      </Container>
+    </Navbar>
   )
 }
 
