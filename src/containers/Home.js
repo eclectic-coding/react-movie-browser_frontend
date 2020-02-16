a1f47e7c17113ebff83b99943769a84dfbb94666
eclecticcoding <chuck@eclecticsaddlebag.com>
2020-02-16 18:30:16 -0500

    Style a home page element with flexbox banner element and full height image background.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,31 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+// Styling Components
+import { Button, Container } from 'react-bootstrap'
+import bgImg from '../assets/images/bg.jpg'
 
 const Home = () => {
   return (
-    <div>
-      <Container>
-        <h2>Home Container</h2>
+    <div
+      className="min-vh-100"
+      style={{
+        background: `url(${bgImg}) no-repeat center center fixed`,
+        backgroundCover: 'cover !important'
+      }}
+    >
+      <Container style={{ paddingTop: '100px' }}>
+        <div className="banner">
+          <div className="banner-text">
+            <h1 className="text-warning">Movie Portal</h1>
+            <h3>Your personal entertainment portal.</h3>
+            <Button variant="warning" className="mt-3">
+              <Link to="/movies" style={{ color: 'inherit' }}>
+                Find a movie
+              </Link>
+            </Button>
+          </div>
+        </div>
       </Container>
     </div>
   )
