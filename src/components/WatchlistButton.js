f617cbb3da4488d30650a284647d2456a0ab698a
eclecticcoding <chuck@eclecticsaddlebag.com>
2020-02-17 17:59:08 -0500

    Add condition tooltip on watchlist button reminding the user to login first.

--- src/components/WatchlistButton.js
+++ src/components/WatchlistButton.js
@@ -1,21 +1,71 @@
 import React, { Component } from 'react'
 // Styling Components
-import { Button } from 'react-bootstrap'
+import { Button, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookmark } from '@fortawesome/free-solid-svg-icons'
+import { connect } from 'react-redux'
+import { getWatchlist } from '../actions/watchlistActions'
 
 class WatchlistButton extends Component {
+  componentDidMount() {
+    const { currentUser, watchList } = this.props
+    // const userId = currentUser.data.id
+    // if (userId && !watchList) {
+    //   console.log('Good job - IF')
+    // }
+  }
+  handleWatchlistClick = e => {
+    const { movie, currentUser } = this.props
+    if (!currentUser.data) {
+      console.log(`Nice click you NOBODY`)
+    }
+  }
+
+  renderTooltip(props) {
+    return <Tooltip {...props}>Please sign in to add movie to watchlist</Tooltip>
+  }
+
   render() {
-    console.log(this.props)
     return (
       <div>
-        <Button variant="success">
-          <FontAwesomeIcon icon={faBookmark} className="mr-2" />
-          Add to my Watchlist
-        </Button>
+        {this.props.currentUser.data ? (
+          <Button
+            variant="success"
+            id="watchlist-btn"
+            onClick={e => this.handleWatchlistClick(e)}
+          >
+            <FontAwesomeIcon icon={faBookmark} className="mr-2" />
+            Add to my Watchlist
+          </Button>
+        ) : (
+          <OverlayTrigger
+            placement="right"
+            delay={{ show: 250, hide: 400 }}
+            overlay={this.renderTooltip}
+          >
+            <Button
+              variant="success"
+              id="watchlist-btn"
+              onClick={e => this.handleWatchlistClick(e)}
+            >
+              <FontAwesomeIcon icon={faBookmark} className="mr-2" />
+              Add to my Watchlist
+            </Button>
+          </OverlayTrigger>
+        )}
       </div>
     )
   }
 }
 
-export default WatchlistButton
+const mapDispatchToProps = dispatch => ({
+  getWatchlist: userId => dispatch(getWatchlist(userId))
+})
+
+const mapStateToProps = state => {
+  return {
+    currentUser: state.users.currentUser
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(WatchlistButton)
